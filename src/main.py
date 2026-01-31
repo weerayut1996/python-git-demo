99cd13a8c3b80d43ca1b0d946e9bbdd334534ce1
weerayut <weerayut.itsd@gmail.com>
2026-01-31 15:29:10 +0700

    Revert "Add branch name output"
    This reverts commit 84dc1a1ac5a9f5173d05e72f211b27615a4a70ae.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
+"""
+pytmain.py
+Entry point ของโปรเจกต์ Python
+"""
+
+
 def main():
     print("Hello, Python + Git + VS Code 🚀")
+<<<<<<< HEAD
     print("Branch: feature/print-version")
     print("Pushed from VS Code")
+=======
+>>>>>>> parent of 84dc1a1 (Add branch name output)
 
 
 if __name__ == "__main__":
